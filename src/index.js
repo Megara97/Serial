90fe6b5204a0c7d2729b77a374b9f259f6b61dea
megara7 <mimamito777@gmail.com>
2023-11-23 18:03:28 -0600

    Autoconectado

--- src/index.js
+++ src/index.js
@@ -19,10 +19,12 @@ io.on("connection", (socket) => {
 	console.log(`El cliente con el id ${socket.id} se a conectado`);
 });
 
+/*
 function connectSerial() {
-	const portB1 = new SerialPort({ path: "/dev/ttyACM0", baudRate: 9600 });
-	//const portB1 = new SerialPort({ path: "/COM4", baudRate: 9600 });
-	const portB2 = new SerialPort({ path: "/COM5", baudRate: 9600 });
+	//const portB1 = new SerialPort({ path: "/dev/ttyACM0", baudRate: 9600 });
+	const portB2 = new SerialPort({ path: "/COM3", baudRate: 9600 });
+	const portB1 = new SerialPort({ path: "/COM4", baudRate: 9600 });
+	//const portB2 = new SerialPort({ path: "/COM5", baudRate: 9600 });
 	const parser1 = new ReadlineParser({ delimiter: "\r\n" });
 	portB1.pipe(parser1);
 	const parser2 = new ReadlineParser({ delimiter: "\r\n" });
@@ -48,7 +50,7 @@ function connectSerial() {
 
 	portB1.on("close", () => {
 		console.log("Puerto Serial de la Bascula 1 Cerrado");
-		//setTimeout(connectSerial, 2000);
+		setTimeout(connectSerial, 5000);
 	});
 
 	portB2.on("open", () => {
@@ -57,67 +59,113 @@ function connectSerial() {
 
 	portB2.on("close", () => {
 		console.log("Puerto Serial de la Bascula 1 Cerrado");
-		//setTimeout(connectSerial, 2000);
+		setTimeout(connectSerial, 5000);
 	});
 
-	portB1.on("error", function (err) {
+	portB1.on("error", (err) => {
 		console.log(err.message);
-		//setTimeout(connectSerial, 2000);
+		setTimeout(connectSerial, 5000);
 	});
 
 	portB2.on("error", function (err) {
 		console.log(err.message);
-		//setTimeout(connectSerial, 2000);
+		setTimeout(connectSerial, 5000);
+	});
+}
+*/
+/*
+function connectSerial1() {
+	const portB1 = new SerialPort({ path: "/COM3", baudRate: 9600 });
+	const parser1 = new ReadlineParser({ delimiter: "\r\n" });
+	portB1.pipe(parser1);
+	
+	parser1.on("data", (data) => {
+		console.log("Respuesta B1: ", data.toString());
+		io.emit("scale1:server", {
+			data: data.toString(),
+		});
+	});
+
+	portB1.on("open", () => {
+		console.log("Puerto Serial de la Bascula 1 Abierto");
+	});
+
+	portB1.on("close", () => {
+		console.log("Puerto Serial de la Bascula 1 Cerrado");
+		setTimeout(connectSerial1, 5000);
+	});
+
+	portB1.on("error", (err) => {
+		console.log('Error Bascula 1',err.message);
+		setTimeout(connectSerial1, 5000);
 	});
 }
 
-//connectSerial();
+function connectSerial2() {
+	const portB2 = new SerialPort({ path: "/COM4", baudRate: 9600 });
+	const parser2 = new ReadlineParser({ delimiter: "\r\n" });
+	portB2.pipe(parser2);
+	
+	parser2.on("data", (data) => {
+		console.log("Respuesta B2: ", data.toString());
+		io.emit("scale2:server", {
+			data: data.toString(),
+		});
+	});
 
-function connectSerialPort(COM, connected) {
-	//const port = new SerialPort({ path: "/COM4", baudRate: 9600 });
-	const port = new SerialPort({ path: COM, baudRate: 9600 });
-	const parser = new ReadlineParser({ delimiter: "\r\n" });
+	portB2.on("open", () => {
+		console.log("Puerto Serial de la Bascula 2 Abierto");
+	});
+
+	portB2.on("close", () => {
+		console.log("Puerto Serial de la Bascula 2 Cerrado");
+		setTimeout(connectSerial2, 5000);
+	});
+
+	portB2.on("error", (err) => {
+		console.log('Error Bascula 2',err.message);
+		setTimeout(connectSerial2, 5000);
+	});
+}
+
+connectSerial1();
+connectSerial2();
+*/
+
+function connectSerialPort(port,parser,COM) {
+	port = new SerialPort({ path: COM, baudRate: 9600 });
+	parser = new ReadlineParser({ delimiter: "\r\n" });
 	port.pipe(parser);
 
 	parser.on("data", (data) => {
-		console.log("Respuesta: ", data.toString());
+		console.log(`Respuesta ${COM}:`, data.toString());
 		io.emit("scale3:server", {
 			data: data.toString(),
 		});
 	});
 
 	port.on("open", () => {
-		console.log("Puerto Serial Abierto");
-		/*setTimeout(function () {
-			// Tu código aquí
-			console.log("Han pasado 1000 milisegundos");
-			//port.write('R')
-			//port.write(String.fromCharCode(82))
-			port.write(Buffer.from("52", "hex"), (err) => {
-				if (err) {
-					console.error("Error al enviar comando:", err.message);
-				} else {
-					console.log("Comando enviado con éxito");
-				}
-			});
-		}, 1000);*/
-		connected = true;
+		console.log(`Puerto Serial ${COM} Abierto`);
 	});
 
 	port.on("close", () => {
-		console.log("Conexión cerrada");
+		console.log(`Conexión ${COM} cerrada`);
+		//console.log(port);
+		setTimeout(() => connectSerialPort(port,parser,COM), 5000);
 	});
 
 	port.on("error", (err) => {
-		console.log(err.message);
+		console.log(`Error ${COM}:`,err.message);
+		setTimeout(() => connectSerialPort(port,parser,COM), 5000);
 	});
 }
 
-let isConnected = false;
-if (isConnected === false) {
-	//console.log("aqui");
-	connectSerialPort("/dev/ttyACM0", isConnected);
-}
+let port1;
+let parser1;
+let port2;
+let parser2;
+connectSerialPort(port1,parser1,"/COM3");
+connectSerialPort(port2,parser2,"/COM4");
 
 /*socketController.scaleChannelListening((data) => {
 	scaleController.requestToScale(portB1, data);
