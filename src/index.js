e5783631e6adb362028d16e0edbfa8ee3760a127
megara7 <mimamito777@gmail.com>
2023-11-27 15:22:12 -0600

    Envio de datos al socket

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import express from "express";
-import http from "http";
-import { Server as WebSocketServer } from "socket.io";
-import { SerialPort, ReadlineParser } from "serialport";
-//import socketController from "./controllers/socket.controllers";
-//import scaleController from "./controllers/scale.controllers";
-import IndexRoutes from "./routes/index.routes";
+import express from 'express';
+import http from 'http';
+import {Server as WebSocketServer} from 'socket.io';
+import {SerialPort, ReadlineParser} from 'serialport';
+//import socketController from './controllers/socket.controllers';
+//import scaleController from './controllers/scale.controllers';
+import IndexRoutes from './routes/index.routes';
 
 const app = express();
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 const server = http.createServer(app);
 const io = new WebSocketServer(server, {
-	cors: {
-		origin: "*",
-	},
+  cors: {
+    origin: '*',
+  },
 });
 
-io.on("connection", (socket) => {
-	console.log(`El cliente con el id ${socket.id} se a conectado`);
+io.on('connection', socket => {
+  console.log(`El cliente con el id ${socket.id} se a conectado`);
 });
 
 /*
@@ -132,66 +132,103 @@ connectSerial1();
 connectSerial2();
 */
 let weight;
+let signo;
 
-function connectSerialPort(port,parser,COM) {
-	port = new SerialPort({ path: COM, baudRate: 9600 });
-	parser = new ReadlineParser({ delimiter: "\r\n" });  CR + LF
-	//parser = new ReadlineParser({ delimiter: "\r" }); CR
-	//parser = new ReadlineParser({ delimiter: "\x03" }); ETX
-	//parser = new ReadlineParser({ delimiter: "\x03" }); EOT
-	//parser = new ReadlineParser({ delimiter: "\x02" }); STX
-	//parser = new ReadlineParser({ delimiter: "\x02" }); S0H
+function connectSerialPort(port, parser, COM) {
+  const scale = COM === '/COM5' ? 'Bascula 1' : 'Bascula 2';
+  port = new SerialPort({path: COM, baudRate: 9600});
+  parser = new ReadlineParser({delimiter: 'g\r\n'});
+  //parser = new ReadlineParser({ delimiter: "\r\n" });  //CR + LF
+  //parser = new ReadlineParser({ delimiter: "\r" }); //CR
+  //parser = new ReadlineParser({ delimiter: "\n" }); //LF
+  port.pipe(parser);
 
-	port.pipe(parser);
+  parser.on('data', data => {
+    console.log(`Respuesta ${scale}:`, data.toString()); //data.toString()
+    /*console.log(`Respuesta ${scale}:`);
+		// Iterar sobre cada carácter e imprimir su código ASCII
+		for (let i = 0; i < data.length; i++) {
+		  const codigoAscii = data.charCodeAt(i);
+		  console.log(`Carácter ${i + 1}: ${data[i]} (Código ASCII: ${codigoAscii})`);
+		}*/
 
-	parser.on("data", (data) => {
-		console.log(`Respuesta ${COM}:`, data.toString());
-		weight = value.substring(5, 14); //Modo  RS Prt
-		weight = value.substring(5, 15); //Modo  RS Cor Protocolo 1
-		weight = value.substring(1, 7); //Modo  RS Cor Protocolo 4
-		const weightOK= weight.split('').reverse().join('')
-		//weight= parseFloat(weight)
-		console.log(weight);
-		console.log('invertido',weightOK);
-		io.emit("scale:server", {puerto: COM, 
-			data: data.toString(),
-		});
-	});
+    const indiceTotal = data.indexOf('TOTAL');
+    //console.log('indice',indiceTotal)
+    if (indiceTotal !== -1) {
+      signo = data.substring(indiceTotal + 5, indiceTotal + 6);
+      weight = data.substring(indiceTotal + 6, indiceTotal + 14);
+      weight = signo === '-' ? parseFloat(weight) * -1 : parseFloat(weight);
+      //weight = data.substring(indiceTotal + 5, indiceTotal + 14);
+      //weight = parseFloat(weight);
+    } else {
+      signo = data.substring(5, 6);
+      weight = data.substring(6, 13);
+      weight = signo === '-' ? parseFloat(weight) * -1 : parseFloat(weight);
+      //weight= signo === '-' ? 0 : parseFloat(weight)
+    }
 
-	port.on("open", () => {
-		console.log(`Puerto Serial ${COM} Abierto`);
-	});
+    /* 
+    //Modo  RS Cor Protocolo 1
+    const signo = data.toString().substring(5, 6);
+    weight = data.toString().substring(6, 13);
+    weight = signo === '-' ? parseFloat(weight) * -1 : parseFloat(weight);
+    //weight= signo === '-' ? 0 : parseFloat(weight)
+	*/
 
-	port.on("close", () => {
-		console.log(`Conexión ${COM} cerrada`);
-		//console.log(port);
-		setTimeout(() => connectSerialPort(port,parser,COM), 5000);
-	});
+    /*
+    //Modo  RS Prt
+    const indiceTotal = data.indexOf('TOTAL');
+    weight = data.substring(indiceTotal + 5, indiceTotal + 14);
+    weight = parseFloat(weight);
+	*/
 
-	port.on("error", (err) => {
-		console.log(`Error ${COM}:`,err.message);
-		setTimeout(() => connectSerialPort(port,parser,COM), 5000);
-	});
+    /*
+    //Modo  RS Cor Protocolo 4
+    //weight = data.toString().substring(1, 7);
+    //const weightOK= weight.split('').reverse().join('')
+    //console.log('invertido',weightOK);
+	*/
+
+    console.log(`Peso ${scale}:`, weight);
+    io.emit('scale:server', {puerto: scale, data: weight});
+  });
+
+  port.on('open', () => {
+    console.log(`Puerto Serial ${scale} Abierto`);
+  });
+
+  port.on('close', () => {
+    console.log(`Conexión ${scale} cerrada`);
+    //console.log(port);
+    setTimeout(() => connectSerialPort(port, parser, COM), 5000);
+  });
+
+  port.on('error', err => {
+    console.log(`Error ${scale}:`, err.message);
+    setTimeout(() => connectSerialPort(port, parser, COM), 5000);
+  });
 }
 
 let port1;
 let parser1;
 let port2;
 let parser2;
-connectSerialPort(port1,parser1,"/COM5"); //Bascula 1
-connectSerialPort(port2,parser2,"/COM4"); //Bascula 2
+connectSerialPort(port1, parser1, '/COM5'); //Bascula 1 COM5
+connectSerialPort(port2, parser2, '/COM3'); //Bascula 2 COM6
 
-/*socketController.scaleChannelListening((data) => {
-	scaleController.requestToScale(portB1, data);
-});*/
+/*
+socketController.scaleChannelListening(data => {
+  scaleController.requestToScale(portB1, data);
+});
+*/
 
-server.listen(app.get("port"), () => {
-	console.log("Server on port", app.get("port"));
+server.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
 });
 
 app.use(IndexRoutes);
 
-export { io };
+export {io};
 
 /*
 const port = new SerialPort({ path: "/COM4", baudRate: 9600 });
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,15 @@
-import { Router } from "express";
-import { io } from "../index";
-import path from "path";
+import {Router} from 'express';
+import {io} from '../index';
+import path from 'path';
 
 const router = Router();
 
-router.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "../index.html"));
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+router.get('/1', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 export default router;
